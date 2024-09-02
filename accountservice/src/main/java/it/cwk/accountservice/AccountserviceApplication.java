296b50fd192786cf15fa44cb9b7c7640a895795f
ankoi0310 <huynhvahuuan3620@gmail.com>
2024-09-02 20:26:41 +0700

    feat: create discovery service

--- accountservice/src/main/java/it/cwk/accountservice/AccountserviceApplication.java
+++ accountservice/src/main/java/it/cwk/accountservice/AccountserviceApplication.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class AccountserviceApplication {
 
     public static void main(String[] args) {
